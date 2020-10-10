a5c3fe7bfc4dc0d433f37814f02ecea713a37b06
Andy Walker <walkeraj@gmail.com>
2020-10-10 12:44:37 -0400

    Reorg nodes

--- parser/parser.go
+++ parser/parser.go
@@ -7,33 +7,6 @@ import (
 	"strings"
 )
 
-// NodeType represents the type of a node in the parse tree
-type NodeType int
-
-// Node Types
-const (
-	_ NodeType = iota
-	NodeAnd
-	NodeOr
-	NodeTerminal
-)
-
-// Node is a node in the query parse tree
-type Node struct {
-	NodeType   NodeType
-	Comparison Comparison
-	Left       *Node
-	Right      *Node
-}
-
-// Comparison is an individual comparision operation on a terminal node
-type Comparison struct {
-	Op      string
-	Negated bool
-	Field   []string
-	Values  []string
-}
-
 // ParseError is the exported error type for parsing errors with detailed information as to where they occurred
 type ParseError struct {
 	Inner    error    `json:"inner"`
